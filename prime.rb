a67c67ab6745c7b42272b4c1c9dc3bc9879dca73
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 05:33:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,27 +1,9 @@
-require 'pry'
-def prime?(num)
-
-  num_pos = (num * num) / num
-  true_false = TRUE
-   f = 3
-   while num_pos % 2 == 0
-        
-        num_pos /= 2
-        FALSE
-      end
-    
-  
-    while f * f <= num_pos
-        if num_pos % f == 0
-            FALSE
-            else
-            f += 2 
-          end
-    if num_pos != 1
-      TRUE
+def prime?(integer)
+  test_divisors = (2..(integer - 1)).to_a
+  if test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1 
+    false
+  else
+    true
   end
-  
- end 
-     
-     
+  (test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1) ? false : true
 end
