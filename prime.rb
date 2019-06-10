7ede6c1a90391665f7207db69184e2b615026f2a
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 05:34:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
-def prime?(integer)
-  test_divisors = (2..(integer - 1)).to_a
-  if test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1 
-    false
-  else
-    true
+def prime?(num)
+  return false if num < 2
+  (2..num-1).each do |i|
+    if num % i == 0
+      return false
+    end
   end
-  (test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1) ? false : true
+  true
 end
