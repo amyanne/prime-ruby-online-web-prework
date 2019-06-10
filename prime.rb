e3772587180687f6ae38257246d78c4e67b0c01d
Amy Westcott <amy.westcott@yahoo.com>
2019-06-10 05:33:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+require 'pry'
+def prime?(num)
+
+  num_pos = (num * num) / num
+  true_false = TRUE
+   f = 3
+   while num_pos % 2 == 0
+        
+        num_pos /= 2
+        FALSE
+      end
+    
+  
+    while f * f <= num_pos
+        if num_pos % f == 0
+            FALSE
+            else
+            f += 2 
+          end
+    if num_pos != 1
+      TRUE
+  end
+  
+ end 
+     
+     
+end
